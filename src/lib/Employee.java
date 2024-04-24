e8a130ced43a96a0c64b38a18e342ba751793b9d
adamrafiffaqih <90492899+AdamRFaqih@users.noreply.github.com>
2024-04-24 08:15:25 +0700

    Menyesuaikan Constuctor dan Funtion
    Menyesuaikan constructor dan function get annual income tax dengan penambahan joinedDate

--- src/lib/Employee.java
+++ src/lib/Employee.java
@@ -30,15 +30,13 @@ public class Employee {
 	private List<String> childNames;
 	private List<String> childIdNumbers;
 	
-	public Employee(String employeeId, String firstName, String lastName, String idNumber, String address, int yearJoined, int monthJoined, int dayJoined, boolean isForeigner, boolean gender) {
+	public Employee(String employeeId, String firstName, String lastName, String idNumber, String address, LocalDate joinedDate, boolean isForeigner, boolean gender) {
 		this.employeeId = employeeId;
 		this.firstName = firstName;
 		this.lastName = lastName;
 		this.idNumber = idNumber;
 		this.address = address;
-		this.yearJoined = yearJoined;
-		this.monthJoined = monthJoined;
-		this.dayJoined = dayJoined;
+		this.joinedDate = joinedDate;
 		this.isForeigner = isForeigner;
 		this.gender = gender;
 		
@@ -93,8 +91,8 @@ public class Employee {
 		//Menghitung berapa lama pegawai bekerja dalam setahun ini, jika pegawai sudah bekerja dari tahun sebelumnya maka otomatis dianggap 12 bulan.
 		LocalDate date = LocalDate.now();
 		
-		if (date.getYear() == yearJoined) {
-			monthWorkingInYear = date.getMonthValue() - monthJoined;
+		if (date.getYear() == joinedDate.getYear()) {
+			monthWorkingInYear = date.getMonthValue() - joinedDate.getMonthValue();
 		}else {
 			monthWorkingInYear = 12;
 		}
